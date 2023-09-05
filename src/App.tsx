ec5bbbc8ecc154fb29800e1a9dfcecdce205eb7b
rameshhb37 <rameshhb37@gmail.com>
2023-09-05 22:40:54 +0530

    Feat: simple form with formik validation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SimpleForm from './components/SimpleForm/SimpleForm';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SimpleForm from "./components/SimpleForm/SimpleForm";
+import { Container, Box, Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <SimpleForm />
-    </div>
+    <Box className="formik-form-examples-container" sx={{p: 4, backgroundColor: '#eeecfc2b'}}>
+      <Container fixed>
+        <Typography variant="h5" sx={{textAlign: 'center'}}>Simple Form</Typography>
+        <div className="App">
+          <SimpleForm />
+        </div>
+      </Container>
+      <Container fixed sx={{mt: 6}}>
+        <div className="App">
+          {/* <SimpleForm /> */}
+        </div>
+      </Container>
+    </Box>
   );
 }
 
--- src/components/SimpleForm/SimpleForm.tsx
+++ src/components/SimpleForm/SimpleForm.tsx
@@ -1,15 +1,61 @@
-import { Container, FormControl, TextField } from "@mui/material";
+import { Button, Container, FormControl, TextField } from "@mui/material";
+import { useFormik } from "formik";
 import React from "react";
 
 const SimpleForm = () => {
+  const validate = (values: any) => {
+    const {username, password} = values;
+    const errors: any = {};
+    if (!username) {
+      errors.username = 'Required';
+    }
+
+    if (!password) {
+      errors.password = 'Required';
+    }
+    return errors;
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validate,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  
   return (
-    <Container sx={{width: '30%'}}>
-      <form>
+    <Container sx={{ width: "30%" }}>
+      <form autoComplete="off">
         <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-        <TextField id="username" label="username" variant="outlined" />
+          <TextField 
+          autoComplete="off"
+          id="username" 
+          label="username" 
+          variant="outlined"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+           />
+           <div>{formik.errors.username}</div>
         </FormControl>
         <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-        <TextField id="password" label="password" variant="outlined" />
+          <TextField 
+          autoComplete="off"
+          id="password" 
+          label="password"
+          type="password"
+          variant="outlined"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          />
+          <>{formik.errors.password}</>
+        </FormControl>
+        <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+          <Button variant="contained">Submit</Button>
         </FormControl>
       </form>
     </Container>
